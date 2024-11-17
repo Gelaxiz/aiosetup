41e971393fb5f1109558eacd68f775ec7b5e122f
Gelaxiz <130848366+Gelaxiz@users.noreply.github.com>
2024-11-17 13:07:47 +0200

    Update aio.ps1

--- aio.ps1
+++ aio.ps1
@@ -2,3 +2,4 @@ mkdir C:\Apps
 cd C:\Apps
 Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/releases/download/app/AioSetup.bat" -Destination "C:\apps\Aio.bat"
 Start-Process C:\apps\aio.bat
+exit
