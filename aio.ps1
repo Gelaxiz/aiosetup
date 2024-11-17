575c48617cd95fcc8dee5e94281b81b168f9eb49
Gelaxiz <130848366+Gelaxiz@users.noreply.github.com>
2024-11-18 00:37:47 +0200

    Update aio.ps1

--- aio.ps1
+++ aio.ps1
@@ -1,5 +1,5 @@
-mkdir C:\Apps
-cd C:\Apps
-Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/releases/download/app/AioSetup.bat" -Destination "C:\apps\Aio.bat"
-Start-Process C:\apps\aio.bat
+mkdir "$env:TEMP\Aioappinstaller"
+cd $env:TEMP\Aioappinstaller
+Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/releases/download/app/AioSetup.bat" -Destination "$env:TEMP\Aioappinstaller\Aio.bat"
+Start-Process aio.bat
 exit
