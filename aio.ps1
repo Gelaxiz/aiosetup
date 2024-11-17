28ccc8a651ffb2e4751df4de672661f546ea1ac4
Gelaxiz <130848366+Gelaxiz@users.noreply.github.com>
2024-11-17 13:06:10 +0200

    Update and rename index.html to aio.ps1

--- aio.ps1
+++ aio.ps1
@@ -1,6 +1,4 @@
-<html><body><pre>
 mkdir C:\Apps
 cd C:\Apps
 Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/releases/download/app/AioSetup.bat" -Destination "C:\apps\Aio.bat"
 Start-Process C:\apps\aio.bat
-</pre></body></html>
