05c02ff14729789495789d07b47ec5ee24f9dfe2
Gelaxiz <130848366+Gelaxiz@users.noreply.github.com>
2024-11-18 00:30:44 +0200

    Delete Aio.bat

--- Aio.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title AIO INSTALLER
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-mkdir "C:\Apps"
-cd C:\Apps
-powershell.exe -Command "Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/releases/download/app/AioSetup.bat" -Destination "C:\apps\Aio.bat"
-start Aio.bat
-exit
